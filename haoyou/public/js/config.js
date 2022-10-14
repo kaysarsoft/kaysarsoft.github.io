2530e5c7fa51203b53fa1aee2002f31211c4b6fe
凯撒 <1290361724@qq.com>
2022-10-14 06:13:32 +0000

    update index.html.
    Signed-off-by: 凯撒 <1290361724@qq.com>

---
+++ haoyou/public/js/config.js
@@ -0,0 +1,24 @@
+var pay_list = [
+    {
+    'type':'alipay2',
+	'name':'wx-好友',
+	'text':'打开wx扫一扫',
+	'qr':'https://www.helloimg.com/images/2022/01/30/G80GAS.md.jpg',
+    'pass':'',
+  },
+    {
+    'type':'qq',
+	'name':'QQ-好友',
+	'text':'打开QQ扫一扫',
+	'qr':'https://www.helloimg.com/images/2022/01/30/G80MNh.md.jpg',
+    'pass':'',
+  },
+  {
+    'type':'wechat',
+	'name':'钉钉-好友',
+	'text':'打开钉钉扫一扫',
+	'qr':'https://www.helloimg.com/images/2022/01/30/G802uc.md.jpg',
+    'pass':'',
+  },
+
+];
