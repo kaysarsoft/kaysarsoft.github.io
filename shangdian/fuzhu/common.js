bdcece525957743eab06c51604c52739efbe68cc
凯撒 <1290361724@qq.com>
2024-05-15 01:53:44 +0000

    删除文件 shangdian

--- shangdian/fuzhu/common.js
+++
@@ -1,4 +0,0 @@
-$(".index_banner").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"left",autoPlay:true});
-$(".g2_slide").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftMarquee",vis:3,interTime:50,autoPlay:true});
-$(".g7_slide").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftMarquee",vis:5,interTime:50,autoPlay:true});
-var gallery = $('.g4_list ul').gallerify({margin:5,mode:'default',lastRow:'adjust'});
