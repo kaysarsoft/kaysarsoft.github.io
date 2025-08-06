829df2e84bc5209c8b838a15332985894b27b071
kaysarsoft <1290361724@qq.com>
2025-08-06 18:48:56 +0800

    更新文件

--- pay/public/js/public.js
+++ pay/public/js/public.js
@@ -4,26 +4,25 @@ function getUrlParam(name) {
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]); return null;
 }
-$(document).ready(function(){
+$(document).ready(function () {
     //选项卡切换
-    $('.tab').live('click',function () {
+    $('.tab').live('click', function () {
         $(this).addClass('pressed').siblings().removeClass('pressed');
         $($(this).attr('tab')).show().siblings().hide();
     });
     //密码保护
-    $('.pass_box input[type=button]').live('click',function () {
+    $('.pass_box input[type=button]').live('click', function () {
         var box = $(this).parents('.pass_box');
-        if ( box.data('pass') == box.find('input[type=text]').val()){
+        if (box.data('pass') == box.find('input[type=text]').val()) {
             box.find('.pass_content').show();
             box.find('.pass_input').hide();
-        }else{
+        } else {
             alert('密码错误!');
         }
     });
     //响应回车
-    $('.pass_box input[type=text]').live('keypress',function(event){
-        if(event.keyCode == 13)
-        {
+    $('.pass_box input[type=text]').live('keypress', function (event) {
+        if (event.keyCode == 13) {
             var box = $(this).parents('.pass_box');
             box.find('input[type=button]').click();
         }
@@ -34,16 +33,16 @@ $(document).ready(function(){
     var tab_main_html = '<div class="tab_main">';
     //输入框
     $.each(pay_list, function (index, obj) {
-        if (obj.qr){
+        if (obj.qr) {
             obj.qr = '<img class="qrcode" src="' + obj.qr + '" />';
         }
-        if (obj.text){
+        if (obj.text) {
             obj.text = '<h2>' + obj.text + '</h2>';
         }
         head_html = '<span id="t_' + obj.type + '" tab="#tab_' + obj.type + '" class="tab">' + obj.name + '</span>';
         main_html = obj.text + obj.qr;
-        if (obj.pass){
-            main_html = "<div data-pass='"+ obj.pass +"' class='pass_box'><div class='pass_input'><input type='text' placeholder='请输入访问密码' class='input' /><input type='button' class='btn' value='提交' /></div><div class='pass_content'>" + main_html + "</div></div>"
+        if (obj.pass) {
+            main_html = "<div data-pass='" + obj.pass + "' class='pass_box'><div class='pass_input'><input type='text' placeholder='请输入访问密码' class='input' /><input type='button' class='btn' value='提交' /></div><div class='pass_content'>" + main_html + "</div></div>"
         }
         tab_head_html = tab_head_html + head_html;
         tab_main_html = tab_main_html + '<div id="tab_' + obj.type + '" class="hidden">' + main_html + '</div>';
@@ -57,7 +56,7 @@ $(document).ready(function(){
     $('#t_' + pay_type).click();
     //传递了密码
     pass = getUrlParam('pass');
-    if(pass){
+    if (pass) {
         $('#tab_' + pay_type).find('input[type=text]').val(pass);
         $('#tab_' + pay_type).find('input[type=button]').click();
     }
