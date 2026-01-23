464e978463323dca92945bddfcb2211d24d71e91
kaysarsoft <1290361724@qq.com>
2026-01-23 12:25:55 +0800

    修复全国教师管理系统各省入口网址

--- vip/teacher/js/script.js
+++ vip/teacher/js/script.js
@@ -3,238 +3,238 @@ const provincesData = [
     {
         id: 1,
         name: "北京市",
-        url: "https://www.bjedu.cn/teacher",
+        url: "https://teacher.bjedu.cn",
         region: "华北",
         description: "北京市教师管理系统官网入口，提供教师注册、信息查询等服务。"
     },
     {
         id: 2,
         name: "天津市",
-        url: "https://www.tjedu.gov.cn/teacher",
+        url: "https://teacher.tjedu.gov.cn",
         region: "华北",
         description: "天津市教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 3,
         name: "河北省",
-        url: "https://www.hebeijiaoyu.com/teacher",
+        url: "https://teacher.hebeijiaoyu.com",
         region: "华北",
         description: "河北省教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 4,
         name: "山西省",
-        url: "https://www.sxedu.gov.cn/teacher",
+        url: "https://teacher.sxedu.gov.cn",
         region: "华北",
         description: "山西省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 5,
         name: "内蒙古自治区",
-        url: "https://www.nmgjyt.gov.cn/teacher",
+        url: "https://teacher.nmgjyt.gov.cn",
         region: "华北",
         description: "内蒙古自治区教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 6,
         name: "辽宁省",
-        url: "https://www.lnjy.com.cn/teacher",
+        url: "https://teacher.lnjy.com.cn",
         region: "东北",
         description: "辽宁省教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 7,
         name: "吉林省",
-        url: "https://www.jledu.gov.cn/teacher",
+        url: "https://teacher.jledu.gov.cn",
         region: "东北",
         description: "吉林省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 8,
         name: "黑龙江省",
-        url: "https://www.hljedu.gov.cn/teacher",
+        url: "https://teacher.hljedu.gov.cn",
         region: "东北",
         description: "黑龙江省教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 9,
         name: "上海市",
-        url: "https://www.shanghaiedu.cn/teacher",
+        url: "https://teacher.shanghaiedu.cn",
         region: "华东",
         description: "上海市教师管理系统官网入口，提供教师注册、信息查询等服务。"
     },
     {
         id: 10,
         name: "江苏省",
-        url: "https://www.jiangsuedu.com/teacher",
+        url: "https://teacher.jiangsuedu.com",
         region: "华东",
         description: "江苏省教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 11,
         name: "浙江省",
-        url: "https://www.zjedu.gov.cn/teacher",
+        url: "https://teacher.zjedu.gov.cn",
         region: "华东",
         description: "浙江省教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 12,
         name: "安徽省",
-        url: "https://www.ahjy.gov.cn/teacher",
+        url: "https://teacher.ahjy.gov.cn",
         region: "华东",
         description: "安徽省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 13,
         name: "福建省",
-        url: "https://www.fjedu.gov.cn/teacher",
+        url: "https://teacher.fjedu.gov.cn",
         region: "华东",
         description: "福建省教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 14,
         name: "江西省",
-        url: "https://www.jxedu.gov.cn/teacher",
+        url: "https://teacher.jxedu.gov.cn",
         region: "华东",
         description: "江西省教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 15,
         name: "山东省",
-        url: "https://www.sdedu.gov.cn/teacher",
+        url: "https://teacher.sdedu.gov.cn",
         region: "华东",
         description: "山东省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 16,
         name: "河南省",
-        url: "https://www.haedu.gov.cn/teacher",
+        url: "https://teacher.haedu.gov.cn",
         region: "华中",
         description: "河南省教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 17,
         name: "湖北省",
-        url: "https://www.hbedu.gov.cn/teacher",
+        url: "https://teacher.hbedu.gov.cn",
         region: "华中",
         description: "湖北省教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 18,
         name: "湖南省",
-        url: "https://www.hnedu.gov.cn/teacher",
+        url: "https://teacher.hnedu.gov.cn",
         region: "华中",
         description: "湖南省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 19,
         name: "广东省",
-        url: "https://www.gdedu.gov.cn/teacher",
+        url: "https://teacher.gdedu.gov.cn",
         region: "华南",
         description: "广东省教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 20,
         name: "广西壮族自治区",
-        url: "https://www.gxedu.gov.cn/teacher",
+        url: "https://teacher.gxedu.gov.cn",
         region: "华南",
         description: "广西壮族自治区教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 21,
         name: "海南省",
-        url: "https://www.hainanedu.gov.cn/teacher",
+        url: "https://teacher.hainanedu.gov.cn",
         region: "华南",
         description: "海南省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 22,
         name: "重庆市",
-        url: "https://www.cqedu.cn/teacher",
+        url: "https://teacher.cqedu.cn",
         region: "西南",
         description: "重庆市教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 23,
         name: "四川省",
-        url: "https://www.scedu.net/teacher",
+        url: "https://teacher.scedu.net",
         region: "西南",
         description: "四川省教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 24,
         name: "贵州省",
-        url: "https://www.gzsjyt.gov.cn/teacher",
+        url: "https://teacher.gzsjyt.gov.cn",
         region: "西南",
         description: "贵州省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 25,
         name: "云南省",
-        url: "https://www.ynjy.cn/teacher",
+        url: "https://teacher.ynjy.cn",
         region: "西南",
         description: "云南省教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 26,
         name: "西藏自治区",
-        url: "https://www.xzedu.gov.cn/teacher",
+        url: "https://teacher.xzedu.gov.cn",
         region: "西南",
         description: "西藏自治区教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 27,
         name: "陕西省",
-        url: "https://www.snedu.gov.cn/teacher",
+        url: "https://teacher.snedu.gov.cn",
         region: "西北",
         description: "陕西省教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 28,
         name: "甘肃省",
-        url: "https://www.gsedu.gov.cn/teacher",
+        url: "https://teacher.gsedu.gov.cn",
         region: "西北",
         description: "甘肃省教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 29,
         name: "青海省",
-        url: "https://www.qhedu.gov.cn/teacher",
+        url: "https://teacher.qhedu.gov.cn",
         region: "西北",
         description: "青海省教师管理系统官网入口，提供教师资格认证、职称评定等服务。"
     },
     {
         id: 30,
         name: "宁夏回族自治区",
-        url: "https://www.nxedu.gov.cn/teacher",
+        url: "https://teacher.nxedu.gov.cn",
         region: "西北",
         description: "宁夏回族自治区教师管理系统官网入口，提供教师信息管理、继续教育等服务。"
     },
     {
         id: 31,
         name: "新疆维吾尔自治区",
-        url: "https://www.xjedu.gov.cn/teacher",
+        url: "https://teacher.xjedu.gov.cn",
         region: "西北",
         description: "新疆维吾尔自治区教师管理系统官网入口，提供教师招聘、培训等服务。"
     },
     {
         id: 32,
         name: "台湾省",
-        url: "https://www.edu.tw/teacher",
+        url: "https://teacher.edu.tw",
         region: "其他",
         description: "台湾省教师管理系统官网入口，提供教师相关服务。"
     },
     {
         id: 33,
         name: "香港特别行政区",
-        url: "https://www.edb.gov.hk/teacher",
+        url: "https://teacher.edb.gov.hk",
         region: "其他",
         description: "香港特别行政区教师管理系统官网入口，提供教师相关服务。"
     },
     {
         id: 34,
         name: "澳门特别行政区",
-        url: "https://www.dsej.gov.mo/teacher",
+        url: "https://teacher.dsej.gov.mo",
         region: "其他",
         description: "澳门特别行政区教师管理系统官网入口，提供教师相关服务。"
     }
